812e30029efd9e3250098ed8ff1c88a924f01497
Alexander <Snakeage@gmail.com>
2020-05-08 13:58:17 +0300

    added toggleItemHiddenMenu into all header-dropdown in mobile view. Changed margin-boton in menu-item.styles to 0.

--- src/components/header-dropdown-mortage/header-dropdown-mortage.styles.jsx
+++ src/components/header-dropdown-mortage/header-dropdown-mortage.styles.jsx
@@ -1,22 +1,9 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const Container = styled.div`
-  width: 30%;
-  display: flex;
-  flex-direction: column;
-  position: relative;
-
-  @media screen and (max-width: 800px) {
-    width: 100%;
-  }
-`;
-
 export const HeaderDropDownContainer = styled.div`
   padding: 30px 20px 40px 100px;
   display: flex;
-
-  /* justify-content: space-between; */
   min-height: 200px;
   width: 100%;
   position: absolute;
@@ -26,6 +13,7 @@ export const HeaderDropDownContainer = styled.div`
   opacity: 0.9;
 
   @media screen and (max-width: 800px) {
+    height: 100%;
     position: fixed;
     margin-left: 48%;
     width: 50%;
@@ -33,6 +21,17 @@ export const HeaderDropDownContainer = styled.div`
     overflow: scroll;
     flex-direction: column;
     top: 0px;
+  }
+`;
+
+export const Container = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  @media screen and (max-width: 800px) {
+    width: 100%;
   }
 `;
 
